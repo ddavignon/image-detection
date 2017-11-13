1d5d3ae9c90188f3e23718456700a58462e50c87
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-11-12 20:33:54 -0800

    initial commit

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run()
