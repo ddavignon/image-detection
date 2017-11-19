dd03c2722b6e02981de9b980f8c5535673b5faaf
Dustin DAvignon <ddavignon@csumb.edu>
2017-11-19 23:44:35 +0000

    add ports and ip for cloud9

--- application.py
+++ application.py
@@ -82,4 +82,8 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
